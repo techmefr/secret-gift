3b8104e5de594a6bc83488155753f37ddf205b2f
techmefr <compigni.gaetan@gmail.com>
2023-11-23 15:38:30 +0100

    add snow

--- secret-santa/src/main.jsx
+++ secret-santa/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Snowfall from "react-snowfall";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <div style={{ snowflakeCount: 20 }}>
+      <Snowfall />
+      <App />
+    </div>
   </React.StrictMode>,
-)
+  document.querySelector("#app")
+);
